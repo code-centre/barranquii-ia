9e29d68c2e0855c10793e6fbdcc88b0f4cd0cad2
David Aragón <103052450+DaviAragon28@users.noreply.github.com>
2024-03-20 15:49:38 -0500

    Develop (#2)
    * Global styles
    
    * Footer
    
    * Change text
    
    * Cards
    
    * New sections
    
    * New components
    
    * Change max width
    
    * Text to uppercase
    
    * Assets
    
    * Add new sections to home
    
    * copy updates
    
    * Gradient
    
    * Center button
    
    * Width and height
    
    * Marquee sponsors
    
    * Add university
    
    * Change location image
    
    ---------
    
    Co-authored-by: David Aragón <davidaragonandrade@gmail.com>
    Co-authored-by: Anuar Harb <anuarharb@gmail.com>

--- app/sections/Location.tsx
+++ app/sections/Location.tsx
@@ -5,9 +5,18 @@ export default function Location() {
   return (
     <section className="flex flex-col gap-5">
       <div>
-        <h2 className="border- border-principleViolet pl-2 border-l-4 text-lg lg:text-4xl uppercase">
-          ¿Cómo llegar?
-        </h2>
+        <div className="flex justify-between md:justify-start items-center md:gap-14">
+          <h2 className="border- border-principleViolet pl-2 border-l-4 text-lg lg:text-4xl uppercase">
+            ¿Cómo llegar?
+          </h2>
+          <Image
+            className="md:w-[230px]"
+            src="/uni-norte.png"
+            height={47}
+            width={230}
+            alt="Logo de la universidad de la norte"
+          />
+        </div>
         <span className="flex items-center gap-1 mt-2 font-semibold text-gray-400 uppercase">
           <div className="bg-principleViolet rounded-full w-1 h-1"></div>
           Ubicación
@@ -17,13 +26,15 @@ export default function Location() {
           Atlántico
         </p>
       </div>
-      <Image
-        src="/uni-norte.png"
-        height={47}
-        width={150}
-        alt="Logo de la universidad de la norte"
-      />
-      <div className="bg-gray-500 rounded-md h-[300px] md:h-[500px]">
+      <div className="rounded-lg overflow-hidden">
+        <iframe
+          className="w-full"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44308.33713906658!2d-74.83541508186032!3d11.013043655776967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42c6a20b68621%3A0x5c3de7e774806ad1!2sUniversidad%20del%20Norte!5e0!3m2!1ses-419!2sco!4v1710960596048!5m2!1ses-419!2sco"
+          height="600"
+          allowFullScreen={false}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </section>
   );
