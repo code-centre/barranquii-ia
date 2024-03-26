72988123ce2a3f2eb962752ac79b439a4e4ddf3c
David Aragón <davidaragonandrade@gmail.com>
2024-03-26 16:48:32 -0500

    Link

--- app/components/InfoEventGrid.tsx
+++ app/components/InfoEventGrid.tsx
@@ -4,17 +4,18 @@ interface Props {
     date: string,
     time: string,
     location: string
+    href: string
 }
 
 
-export default function InfoEventGrid({date, time, location}: Props) {
+export default function InfoEventGrid({date, time, location, href}: Props) {
   return (
-    <div className="flex flex-col justify-center items-center bg-[#19161c] rounded-md">
+    <a href={href} target="_blank" className="hover:brightness-150 flex flex-col justify-center items-center bg-[#19161c] rounded-md">
       <ul className="flex flex-col gap-2 text-center">
         <li className="uppercase">{date}</li>
         <li className="uppercase">{time}</li>
         <li className="text-sm uppercase">{location}</li>
       </ul>
-    </div>
+    </a>
   );
 }
