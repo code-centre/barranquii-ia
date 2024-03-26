6a7a11f07b382ff6d7abe15f9d6bd683aa3867e1
David Aragón <davidaragonandrade@gmail.com>
2024-03-26 16:22:50 -0500

    Faqs

---
+++ app/sections/FAQ.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ArrowDown } from "../components/Icons";
+import faqs from "../utils/FAQ.json";
+
+export default function FAQ() {
+  return (
+    <div className="pt-20 min-h-screen">
+      <div>
+        <h2 className="border-principleViolet pl-2 border-l-4 font-bold text-lg lg:text-4xl uppercase">
+          FAQs
+        </h2>
+      </div>
+
+      <div className="grid mx-auto mt-8 divide-y divide-neutral-200">
+        {faqs.map((faq, i) => (
+          <div key={i} className="py-5">
+            <details className="group">
+              <summary className="flex justify-between items-center font-semibold text-xl cursor-pointer list-none">
+                <span>{faq.question}</span>
+                <span className="group-open:rotate-180 transition">
+                  <ArrowDown />
+                </span>
+              </summary>
+              <p className="mt-3 text-gray-300 group-open:animate-fadeIn">
+                {faq.answer}
+              </p>
+            </details>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
