6371e2eecb882b604f89b52c0a745d1944892be0
David Aragón <davidaragonandrade@gmail.com>
2024-04-01 12:09:31 -0500

    Change image and title of topics

--- app/components/Topic.tsx
+++ app/components/Topic.tsx
@@ -10,10 +10,11 @@ export default function Topic({ img, title }: Props) {
   return (
     <div>
         <Image 
-            src={img} 
+            src='/question-mark.webp' 
             alt={title} 
             height={160} 
             width={160}  
+<<<<<<< HEAD
 <<<<<<< HEAD
             className='mx-auto w-28 md:w-40 h-28 md:h-40 object-cover'
         />
@@ -23,6 +24,11 @@ export default function Topic({ img, title }: Props) {
         />
         <p className='text-center text-gray-300 w-[150px] mx-auto font-medium font-mono'>{title}</p>
 >>>>>>> 147cc84 (Topics)
+=======
+            className='mx-auto w-28 md:w-40 h-28 md:h-40 object-cover'
+        />
+        <p className='mx-auto w-[150px] font-medium font-mono text-center text-gray-300'>{title}</p>
+>>>>>>> 11fbcb8 (Change image and title of topics)
     </div>
   )
 }
