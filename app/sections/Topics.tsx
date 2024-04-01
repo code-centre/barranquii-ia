11fbcb84a0b4e03a65c59ce967bc388af2550897
David Aragón <davidaragonandrade@gmail.com>
2024-04-01 12:09:31 -0500

    Change image and title of topics

--- app/sections/Topics.tsx
+++ app/sections/Topics.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 import Topic from "../components/Topic";
-import '../globals.css'
+import "../globals.css";
 
 export default function Topics() {
   return (
     <section>
-      <h2 className="border- font-bold border-principleViolet pl-2 border-l-4 text-lg lg:text-4xl uppercase">
+      <h2 className="border- border-principleViolet pl-2 border-l-4 font-bold text-lg lg:text-4xl uppercase">
         Lineas temáticas
       </h2>
 
-      <p className="text-gray-300 mt-3">
+      <p className="mt-3 text-gray-300">
         En esta sección, descubre los diferentes desafíos que te esperan en el
         Hackathon. Cada línea temática representa un área de enfoque donde
         pondrás a prueba tu creatividad, habilidades e ingenio para desarrollar
         soluciones innovadoras.
       </p>
 
-      <div className="mt-10 flex flex-wrap justify-center items-center gap-5 md:gap-10">
-        <Topic img="/cities.webp" title="Ciudades inteligentes" />
-        <Topic img="/health.webp" title="Salud digital" />
-        <Topic img="/education.webp" title="Educación personalizada" />
-        <Topic img="/environment.webp" title="Sostenibilidad ambiental" />
-        <Topic img="/assistant.webp" title="Asistente virtual" />
-        <Topic img="/entertainment.webp" title="Entretenimiento y creatividad" />
-        <Topic img="/security.webp" title="Seguridad y privacidad" />
+      <div className="flex flex-wrap justify-center items-center gap-5 md:gap-10 mt-10">
+        <Topic img="/cities.webp" title="8 de Abril" />
+        <Topic img="/health.webp" title="8 de Abril" />
+        <Topic img="/education.webp" title="15 de Abril" />
+        <Topic img="/environment.webp" title="15 de Abril" />
+        <Topic img="/assistant.webp" title="22 de Abril" />
+        <Topic img="/entertainment.webp" title="22 de Abril" />
+        <Topic img="/security.webp" title="22 de Abril" />
       </div>
     </section>
   );
