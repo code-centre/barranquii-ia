1b6c699f849de72029a165795c7835808c7d21eb
David Aragón <davidaragonandrade@gmail.com>
2024-04-09 09:46:44 -0500

    Terms and conditions

---
+++ app/tickets/terminos-y-condiciones/page.tsx
@@ -0,0 +1,154 @@
+import Wrapper from "@/app/components/Wrapper";
+import React from "react";
+
+export default function TermsAndConditions() {
+  return (
+    <main className="min-h-screen pt-32">
+      <Wrapper styles="px-6">
+        <h1 className="text-5xl font-bold mb-8 text-center">
+          Términos y condiciones
+        </h1>
+        <ol className="max-w-3xl mx-auto flex flex-col gap-6 pb-20 list-decimal">
+          <li className="text-gray-300">
+            <span className="font-bold text-white">Distintivos:</span> Los
+            asistentes deben presentar su documento de identidad y el distintivo
+            (bagde de asistente o speaker) en la portería de la Universidad del
+            Norte y en el área de registro antes de acceder al evento, el
+            distintivo se debe llevar en todo momento en un lugar visible
+            mientras se esté en el evento, durante su estancia dentro de la
+            Universidad y en cualquier actividad asociada al evento. Los
+            distintivos serán personales y no se pueden compartir ni
+            intercambiar con ninguna persona.
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">
+              Pago y y transferencia de boleto:
+            </span>{" "}
+            El pago debe efectuarse en la forma y los plazos indicados por la
+            organización del evento. Se reserva el derecho de cancelar la
+            inscripción sin previo aviso si no se efectúa el pago como
+            corresponda. Las transferencias de boletos están sujetas a las
+            fechas y tarifas especificadas en los distintos medios de
+            información del evento. No se efectuarán reembolso si el usuario no
+            se presenta al evento. Las solicitudes de transferencias de boleto
+            deberán ser realizadas por escrito al correo{" "}
+            <span className="text-principleViolet">
+              contacto@fundacioncodigoabierto.com
+            </span>
+            . Los pagos son finales, no reembolsables y válidos exclusivamente
+            para el evento especificado. La inscripción al evento está sujeta a
+            la disponibilidad.
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">
+              Seguridad y pertenencias:
+            </span>{" "}
+            Barranqui-IA se reserva el derecho de tomar sin previo aviso
+            cualquier medida de seguridad que considere necesaria para aumentar
+            la seguridad de los asistentes y expositores y no se responsabiliza
+            por la pérdida de objetos, los asistentes se ajustarán a la
+            delimitación espacial del evento y aquellos que pongan en riesgo la
+            seguridad del evento serán expulsados.
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">Asistentes nocturnos:</span>{" "}
+            Los asistentes que tomen la opción de permanecer en el evento
+            trabajando en sus proyectos durante la noche del sábado 4 de mayo
+            hasta el domingo 5 de mayo , aceptan y se ajustan a la delimitación
+            espacial del evento y a la condición de no salir ni ingresar a la
+            Universidad del Norte a partir de 9:00 pm y hasta las 6:00 am del
+            dia siguiente
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">Edad mínima:</span> Para
+            inscribirse al evento es obligatorio haber cumplido al menos 16 años
+            de edad. Los asistentes menores de edad deben anexar carta de
+            permiso de padres o tutores, quienes asumen la responsabilidad de su
+            asistencia.
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">Conducta en el evento:</span>{" "}
+            Barranqui- IA se reserva el derecho de negar la admisión o expulsar
+            a cualquier persona cuya conducta sea peligrosa, perturbadora,
+            acosadora, vulgar, obscena, indecente, difamatoria, de incitación al
+            odio u ofensiva (lo que incluye declaraciones u opiniones
+            denigrantes en cuanto a la raza, el género o el origen étnico o
+            condición) No se permitirá el ingreso de bebidas alcohólicas ni
+            personas en estado de embriaguez, sustancias alucinógenas, armas u
+            objetos cortopunzantes.
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">
+              {" "}
+              Información personal y grabaciones:
+            </span>{" "}
+            Barranqui-IA se ocupará de la información personal recopilada
+            durante la inscripción de acuerdo con la declaración de privacidad
+            del evento. Al inscribirse al Hackaton, el usuario acepta y da su
+            consentimiento irrevocable a que Barranqui-IA tenga derecho de
+            grabar, transcribir, modificar, representar, reproducir, mostrar,
+            distribuir y transmitir en cualquier forma y con cualquier propósito
+            su nombre, el nombre de su empresa, su voz, su imagen, su
+            fotografía, su retrato y otras reproducciones de su aspecto (de
+            forma colectiva, su “imagen”) en el evento, en relación con
+            cualquier material y contenido publicitario o promocional, y para
+            cualquier otro propósito legal —por ejemplo, la publicación en
+            sitios web públicos, sin ninguna compensación para el usuario. Sin
+            embargo, no está obligado a hacer ningún uso de la imagen del
+            usuario ni a ejercer ninguno de los derechos que otorgue. El usuario
+            también acepta que su imagen pueda aparecer en fotografías del
+            evento disponibles tanto para el propio usuario como para otros
+            asistentes al evento y el público, y puede conservar y distribuir
+            esas fotos.
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">
+              Información de inscripción:
+            </span>{" "}
+            El usuario acepta que Barranqui-IA pueda utilizar su información de
+            inscripción para enviarle actualizaciones y encuestas relacionadas
+            con el evento. También acepta que pueda compartir su información
+            personal con terceros prestatarios de servicios para tramitar su
+            inscripción. Tales terceros pueden tener sus propias políticas de
+            privacidad aplicables al tratamiento de la información personal, el
+            participante autoriza a Barranqui-IA a compartir la información de
+            su inscripción con actores en particular y acepta recibir
+            comunicaciones de dicho prestador o socio, que están sujetas a las
+            prácticas de privacidad del prestador o socio, incluida la
+            declaración o política de privacidad de la entidad. Al asistir al
+            evento, el usuario da su consentimiento expreso Barranqui-IA y a sus
+            agentes para fotografiar y grabar el evento, así como las
+            actividades relacionadas con el evento, en medios visuales y de
+            audio.
+          </li>
+          <li className="text-gray-300">
+            <span className="font-bold text-white">Acuerdo:</span> Se cancelará
+            la inscripción de los asistentes que incumplan con lo dispuesto en
+            estos términos y condiciones.
+          </li>
+        </ol>
+
+        <h3 className="text-3xl font-bold mb-8 text-center">
+          Declaración de privacidad
+        </h3>
+        <div className="max-w-3xl mx-auto pb-20 text-gray-300">
+          <p className="mb-4">
+            Nuestra declaración de privacidad está basada en Ley de Protección
+            de Datos Personales o Ley 1581 de 2012.
+          </p>
+          <p>
+            Contamos con medidas de seguridad de la información en los procesos,
+            trámites, servicios, sistemas de información y su infraestructura
+            permitiendo preservar la confidencialidad, integridad,
+            disponibilidad y privacidad de los datos y la información que se
+            administran en este, y en cumplimiento del marco jurídico
+            correspondiente, con el objetivo de proporcionar una experiencia
+            confiable, mediante un servicio seguro. Los datos de los asistentes
+            sólo serán compartidos con actores que mantengan las mismas
+            políticas de seguridad y protección de datos e información
+          </p>
+        </div>
+      </Wrapper>
+    </main>
+  );
+}
