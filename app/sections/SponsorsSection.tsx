65b5212ee19cb9eaed82e4016aef711d3e414771
David Aragón <davidaragonandrade@gmail.com>
2024-04-24 21:08:33 -0500

    Change ui

--- app/sections/SponsorsSection.tsx
+++ app/sections/SponsorsSection.tsx
@@ -10,8 +10,66 @@ export default function SponsorsSection() {
       <h2 className="border- border-principleViolet pl-2 border-l-4 font-bold text-lg lg:text-4xl uppercase">
         Patrocinadores
       </h2>
-
-      <Sponsors />
+      <ul className="flex flex-col items-center justify-center mt-10 md:flex-wrap md:flex-row gap-10 md:gap-20">
+        <li>
+          <Image
+            className="w-[200px] h-[60px]"
+            src="/google.png"
+            height={47}
+            width={150}
+            alt="Logo de Google"
+          />
+        </li>
+        <li>
+          <Image
+            className="w-[150px]"
+            src="/i-city.webp"
+            height={47}
+            width={150}
+            alt="Logo de Immersive city"
+            loading="lazy"
+          />
+        </li>
+        <li>
+          <Image
+            className="w-[230px]"
+            src="/uni-norte.png"
+            height={47}
+            width={230}
+            alt="Logo de la universidad de la norte"
+            loading="lazy"
+          />
+        </li>
+        <li>
+          <figure className="flex items-center justify-center gap-x-2">
+            <Image
+              className="w-12"
+              src="/logo-cc.webp"
+              height={100}
+              width={150}
+              alt="Logo de Code Centre"
+              loading="lazy"
+            />
+            <Image
+              className="w-[150px]"
+              src="/cc.webp"
+              height={19}
+              width={150}
+              alt="Logo de Code Centre"
+              loading="lazy"
+            />
+          </figure>
+        </li>
+        <li>
+          <Image
+            className="w-[250px] h-[60px] object-contain"
+            src="/logos/gorilla.webp"
+            height={47}
+            width={150}
+            alt="Logo de Gorilla Logic"
+          />
+        </li>
+      </ul>
     </section>
   );
 }
