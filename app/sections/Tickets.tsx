15350bdbbcba98e62b69d735e22c1ebaec047892
Anuar Harb <anuarharb@gmail.com>
2024-04-29 08:12:56 -0500

    ticket update

--- app/sections/Tickets.tsx
+++ app/sections/Tickets.tsx
@@ -33,9 +33,7 @@ export default function Tickets() {
           amount="40,000"
         >
           <ul className="mt-3 ml-5 marker:text-principleViolet list-disc">
-            <li>
-              Acceso a 6 talleres exclusivos sobre IA esté sábado 4 de Mayo
-            </li>
+            <li>Acceso a 6 talleres exclusivos sobre IA</li>
             <li>Hidratación y café ilimitado</li>
             <li>Certificación de participación</li>
           </ul>
