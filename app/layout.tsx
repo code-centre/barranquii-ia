09f76bd4151995d2b38705e0ad2fb84628bd76f6
David Aragón <davidaragonandrade@gmail.com>
2024-05-02 10:38:02 -0500

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,21 @@ const noto = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Barranqui-IA",
-  description: "Primer hackatón de IA en el Caribe",
+  description: "Barranqui-IA 2024: Un evento único en Barranquilla dedicado a la innovación y la tecnología. Únete a nosotros para experimentar talleres interactivos, un emocionante hackatón y la oportunidad de conectar con líderes en inteligencia artificial y desarrollo tecnológico. Aprende, innova y crea con los mejores en el campo.",
+  authors: [{name: 'Fundación Código Abierto', url: 'https://fundacioncodigoabierto.com/'}],
+  keywords: ['Barranqui-IA', 'hackatón en Barranquilla', 'taller de tecnología', 'inteligencia artificial', 'innovación tecnológica', 'evento de TI', 'desarrollo de software', 'comunidad tecnológica'],
+  openGraph: {
+    type: "website",
+    url: "https://www.barranquiia.com",
+    title: "Barranqui-IA 2024",
+    description: "Participa en el principal evento de tecnología e inteligencia artificial en Barranquilla. Talleres, hackatón y mucho más.",
+    siteName: "Barranqui-IA",
+    images: [{
+      url: "http://www.barranquiia.com/opengraph-image.jpg",
+    }],
+    locale: "es_ES"
+  }
+  
 };
 
 export default function RootLayout({
