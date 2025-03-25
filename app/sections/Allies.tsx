961bd486d03abd18f2956e8ac5a80947fa3ac676
Anuar Harb <anuarharb@gmail.com>
2025-03-25 04:16:18 -0500

    allies section

--- app/sections/Allies.tsx
+++ app/sections/Allies.tsx
@@ -2,167 +2,195 @@ import Image from "next/image";
 import React from "react";
 import "../globals.css";
 import { InfiniteMovingCards } from "../components/InfiniteMovingCards";
-import Wrapper from "../components/Wrapper";
 
 export default function Allies() {
   return (
     <section id="allies" className="scroll-m-32 pb-10 px-10">
       <h2 className="border- border-principleViolet mb-10 pl-2 border-l-4 font-bold text-2xl lg:text-4xl uppercase">
-        Aliados
+        Aliados 2024
       </h2>
-      <div className="flex flex-col md:flex-row md:flex-wrap items-center justify-center gap-14 md:gap-10">
-        <div className="bg-white px-3 py-2 rounded-md">
+      <InfiniteMovingCards direction="left" speed="slow" pauseOnHover={true}>
+        <li className="flex items-center justify-center bg-white px-3 py-2 rounded-md">
           <Image
-            className="w-[200px]"
+            className="w-[150px] h-auto object-contain"
             src="/logos/logo_python_colombia.jpg"
             height={47}
             width={150}
             alt="Logo de Python Colombia"
             loading="lazy"
           />
-        </div>
-        <Image
-          className="w-[200px]"
-          src="/logos/pybaq-logo.webp"
-          height={47}
-          width={150}
-          alt="Logo de Python Barranquilla"
-          loading="lazy"
-        />
-        <Image
-          className=" object-contain w-[260px]"
-          src="/logos/gobernacion.webp"
-          height={47}
-          width={260}
-          alt="Logo de Gobernación del Atlántico"
-          loading="lazy"
-        />
-        <Image
-          className=""
-          src="/logos/innflue.webp"
-          height={47}
-          width={150}
-          alt="Logo de Innflue"
-          loading="lazy"
-        />
-        <div className="bg-white px-3 py-2 rounded-md">
+        </li>
+        <li className="flex items-center justify-center">
           <Image
-            className="w-[200px]"
+            className="w-[150px] h-auto object-contain"
+            src="/logos/pybaq-logo.webp"
+            height={47}
+            width={150}
+            alt="Logo de Python Barranquilla"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/gobernacion.webp"
+            height={47}
+            width={150}
+            alt="Logo de Gobernación del Atlántico"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/innflue.webp"
+            height={47}
+            width={150}
+            alt="Logo de Innflue"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center bg-white px-3 py-2 rounded-md">
+          <Image
+            className="w-[150px] h-auto object-contain"
             src="/logos/startup-huddle.webp"
             height={47}
             width={150}
             alt="Logo de Startup Huddle"
             loading="lazy"
           />
-        </div>
-        <Image
-          className="w-[200px] object-contain"
-          src="/logos/caribe-dev.svg"
-          height={47}
-          width={150}
-          alt="Logo de Caribe Dev"
-          loading="lazy"
-        />
-        <Image
-          className=" rounded-full"
-          src="/logos/51-labs.webp"
-          height={47}
-          width={150}
-          alt="Logo de 51 Labs"
-          loading="lazy"
-        />
-
-        <Image
-          className=" object-cover "
-          src="/logos/cs-blanco.webp"
-          height={47}
-          width={150}
-          alt="Logo de CS"
-          loading="lazy"
-        />
-        <Image
-          className=" rounded-full"
-          src="/logos/fof.webp"
-          height={47}
-          width={150}
-          alt="Logo de Friends of Figma - Barranquilla"
-          loading="lazy"
-        />
-        <Image
-          className="w-[200px] object-contain"
-          src="/logos/logo-prime.webp"
-          height={47}
-          width={150}
-          alt="Logo de Prime - Business School Universidad Sergio Arboleda"
-          loading="lazy"
-        />
-        <Image
-          className="rounded-full"
-          src="/logos/js-community.webp"
-          height={47}
-          width={150}
-          alt="Logo de JS Barranquilla"
-          loading="lazy"
-        />
-        <Image
-          className="w-[200px] object-contain"
-          src="/logos/mangus.webp"
-          height={47}
-          width={150}
-          alt="Logo de Mangus"
-          loading="lazy"
-        />
-        <Image
-          className="w-[250px] object-contain"
-          src="/logos/feanware.png"
-          height={47}
-          width={150}
-          alt="Logo de Feanware"
-          loading="lazy"
-        />
-        
-        <Image
-          className="rounded-full"
-          src="/logos/life-your-coaching.jpg"
-          height={47}
-          width={150}
-          alt="Logo de Life Your Coaching"
-          loading="lazy"
-        />
-        <Image
-          className="rounded-full"
-          src="/logos/aws.png"
-          height={47}
-          width={150}
-          alt="Logo de JS Barranquilla"
-          loading="lazy"
-        />
-
-        <Image
-          className="rounded-full shadow-sm shadow-[#1a1a1a]"
-          src="/logos/linux.webp"
-          height={47}
-          width={150}
-          alt="Logo de Linux Barranquilla"
-          loading="lazy"
-        />
-        <Image
-          className=" rounded-full"
-          src="/logos/wie_group.webp"
-          height={47}
-          width={150}
-          alt="Logo de Wie Group"
-          loading="lazy"
-        />
-        <Image
-          className=""
-          src="/logos/rtb.jpg"
-          height={47}
-          width={150}
-          alt="Logo RTB"
-          loading="lazy"
-        />
-      </div>
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/caribe-dev.svg"
+            height={47}
+            width={150}
+            alt="Logo de Caribe Dev"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain rounded-full"
+            src="/logos/51-labs.webp"
+            height={47}
+            width={150}
+            alt="Logo de 51 Labs"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/cs-blanco.webp"
+            height={47}
+            width={150}
+            alt="Logo de CS"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain rounded-full"
+            src="/logos/fof.webp"
+            height={47}
+            width={150}
+            alt="Logo de Friends of Figma - Barranquilla"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/logo-prime.webp"
+            height={47}
+            width={150}
+            alt="Logo de Prime - Business School Universidad Sergio Arboleda"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain rounded-full"
+            src="/logos/js-community.webp"
+            height={47}
+            width={150}
+            alt="Logo de JS Barranquilla"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/mangus.webp"
+            height={47}
+            width={150}
+            alt="Logo de Mangus"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/feanware.png"
+            height={47}
+            width={150}
+            alt="Logo de Feanware"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain rounded-full"
+            src="/logos/life-your-coaching.jpg"
+            height={47}
+            width={150}
+            alt="Logo de Life Your Coaching"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain rounded-full"
+            src="/logos/aws.png"
+            height={47}
+            width={150}
+            alt="Logo de AWS"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain rounded-full shadow-sm shadow-[#1a1a1a]"
+            src="/logos/linux.webp"
+            height={47}
+            width={150}
+            alt="Logo de Linux Barranquilla"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain rounded-full"
+            src="/logos/wie_group.webp"
+            height={47}
+            width={150}
+            alt="Logo de Wie Group"
+            loading="lazy"
+          />
+        </li>
+        <li className="flex items-center justify-center">
+          <Image
+            className="w-[150px] h-auto object-contain"
+            src="/logos/rtb.jpg"
+            height={47}
+            width={150}
+            alt="Logo RTB"
+            loading="lazy"
+          />
+        </li>
+      </InfiniteMovingCards>
     </section>
   );
 }
