da7734288bcafc2671cc8a8d42c685aeaf4b11aa
David Aragón <davidaragonandrade@gmail.com>
2025-03-31 15:17:14 -0500

    Styles

--- app/[landing]/page.tsx
+++ app/[landing]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function LandingPage({ params }: { params: { landing: string } }) {
   return (
-    <main className="gap-32 xl:gap-44 flex flex-col py-20 min-h-screen overflow-hidden">
+    <main className="gap-20 flex flex-col py-20 min-h-screen">
      <p className='text-2xl text-white'>{params.landing}</p>
     </main>
   )
