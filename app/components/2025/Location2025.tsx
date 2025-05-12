99a9292ad0c295f077779ff7902ae16204b41800
David Aragón <davidaragonandrade@gmail.com>
2025-05-12 09:47:59 -0500

    Remove map

--- app/components/2025/Location2025.tsx
+++ app/components/2025/Location2025.tsx
@@ -26,16 +26,6 @@ export default function Location({ landing }: Props) {
           <p>Cuando cayó la noche, nos movimos a la sede de Fundación Código Abierto, donde la energía no se detuvo. Continuamos trabajando toda la madrugada en un espacio cómodo, creativo y lleno de buena vibra.</p>
           <p>Hubo snacks, café, y dinámicas especiales para mantener la moral en alto. Y sí, algunos participantes trajeron sus almohadas y descansaron unas horas.</p>
           <p>Nos reencontramos en la Universidad del Norte para recargar energías con un desayuno conjunto y preparar los toques finales antes de los pitches y la premiación.</p>
-          <div className="rounded-lg overflow-hidden">
-            <iframe
-              className="w-full"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44308.33713906658!2d-74.83541508186032!3d11.013043655776967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42c6a20b68621%3A0x5c3de7e774806ad1!2sUniversidad%20del%20Norte!5e0!3m2!1ses-419!2sco!4v1710960596048!5m2!1ses-419!2sco"
-              height="300"
-              allowFullScreen={false}
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
         </div>
 
       </div>
